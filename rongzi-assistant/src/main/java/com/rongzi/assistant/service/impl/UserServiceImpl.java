f030daebfe85692937204af5a76fa862996f5195
liujunwei <liujunwei@rongzi.com>
2018-09-07 09:46:09 +0800

    添加 Account.EmpWorkMobile & Account.EmpWechatId 及查询

--- rongzi-assistant/src/main/java/com/rongzi/assistant/service/impl/UserServiceImpl.java
+++ rongzi-assistant/src/main/java/com/rongzi/assistant/service/impl/UserServiceImpl.java
@@ -1,28 +1,43 @@
 package com.rongzi.assistant.service.impl;
 
 import com.rongzi.assistant.model.Account;
+import com.rongzi.assistant.model.City;
 import com.rongzi.assistant.model.UserInfo;
+import com.rongzi.assistant.service.CityService;
 import com.rongzi.assistant.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+    private CityService cityService;
+
     @Override
     public UserInfo getUserInfo(Account account) {
+        List<City> allCitys = cityService.findAllCitys();
+        Map<String, String> cityMap = allCitys.stream().collect(Collectors.toMap(City::getCityCode, City::getCityName));
+
         UserInfo userInfo = new UserInfo();
         userInfo.setAccountName(account.getLgnAccount());
         userInfo.setEmpCode(account.getEmpCode());
         userInfo.setEmpName(account.getEmpDesc());
         userInfo.setCityCode(account.getCityCode());
-        userInfo.setEmpWechatId("wxid_slhv0lqkbac222");
-        userInfo.setEmpWorkMobile("");
+        userInfo.setCityName(cityMap.get(account.getCityCode()));
+        userInfo.setEmpWechatId(account.getEmpWechatId());
+        userInfo.setEmpWorkMobile(account.getEmpWorkMobile());
+
 // TODO
-//        userInfo.setCityName("上海");
 //        userInfo.setDprCode("dev");
 //        userInfo.setDprName("dev");
 //        userInfo.setRoleCode("dev");
 //        userInfo.setRoleName("dev");
+
         return userInfo;
     }
 
