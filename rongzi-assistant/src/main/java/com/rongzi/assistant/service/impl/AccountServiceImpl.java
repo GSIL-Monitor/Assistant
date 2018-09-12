1acf6cdd7137a2162e5df4d16a07a58302490d8c
liujunwei <liujunwei@rongzi.com>
2018-09-12 22:16:46 +0800

    添加登录日志

--- rongzi-assistant/src/main/java/com/rongzi/assistant/service/impl/AccountServiceImpl.java
+++ rongzi-assistant/src/main/java/com/rongzi/assistant/service/impl/AccountServiceImpl.java
@@ -9,6 +9,8 @@ import com.rongzi.assistant.service.AccountService;
 import com.rongzi.core.exception.GunsException;
 import com.rongzi.core.util.DesUtil;
 import com.rongzi.core.util.MD5Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 /**
@@ -22,10 +24,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class AccountServiceImpl extends ServiceImpl<AccountMapper, Account> implements AccountService {
 
+    private Logger logger = LoggerFactory.getLogger(AccountServiceImpl.class);
+
     @Override
     public Account login(String username, String password) throws GunsException {
         EntityWrapper<Account> wrapper = new EntityWrapper<>();
         wrapper.eq("SUBSTRING(EMP_CODE, 3, 10)", username);
+        wrapper.eq("USE_FLAG", 1);
         Account account = selectOne(wrapper);
         if (account == null) {
             throw new GunsException(AssistantExceptionEnum.WRONG_USERNAME_PASSWORD);
@@ -34,11 +39,14 @@ public class AccountServiceImpl extends ServiceImpl<AccountMapper, Account> impl
         String clearText = null;
         try {
             clearText = DesUtil.decrypt(account.getLgnPswd(), "ddddffff");
+            logger.debug("ClearText:" + clearText);
         } catch (Exception e) {
             throw new GunsException(AssistantExceptionEnum.WRONG_USERNAME_PASSWORD);
         }
 
         String truePassword = MD5Util.encrypt(clearText);
+        logger.debug("TruePassword:" + truePassword);
+        logger.debug("InputPassword:" + password + "; UserName:" + username);
         if (!password.equals(truePassword)) {
             throw new GunsException(AssistantExceptionEnum.WRONG_USERNAME_PASSWORD);
         }
