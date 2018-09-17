8e9b91c755c14c2fe3c30cadafdfd85828fa6229
xulei <abc@123.com>
2018-09-17 11:31:42 +0800

    取消手机号码校验

--- rongzi-assistant/src/main/java/com/rongzi/assistant/model/SystemMessageParam.java
+++ rongzi-assistant/src/main/java/com/rongzi/assistant/model/SystemMessageParam.java
@@ -35,7 +35,6 @@ public class SystemMessageParam implements Serializable {
      * 客户手机号码
      */
     @NotBlank
-    @Pattern(regexp = "^(((13[0-9])|(14[579])|(15([0-3]|[5-9]))|(16[6])|(17[0135678])|(18[0-9])|(19[89]))\\d{8})$", message = "手机号码格式不正确")
     private String customerMobile;
 
     /**
