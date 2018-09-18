278306c68b0581c20ef3cae9b608131b48d80b04
liujunwei <liujunwei@rongzi.com>
2018-09-18 15:15:03 +0800

    修复druid监控不能显示sql的问题；删除配置文件中的开发测试环境不存在的数据库

--- rongzi-assistant/src/main/java/com/rongzi/assistant/config/datasource/MybatisPlusConfig.java
+++ rongzi-assistant/src/main/java/com/rongzi/assistant/config/datasource/MybatisPlusConfig.java
@@ -61,19 +61,11 @@ public class MybatisPlusConfig {
                 DataSourceModel dataSourceModel = entry.getValue();
 
                 DruidDataSource druidDataSource = new DruidDataSource();
-//                druidProperties.config(mngDataSource);
+                druidProperties.config(druidDataSource);
                 druidDataSource.setDriverClassName(dataSourceModel.getDriverClassName());
                 druidDataSource.setUrl(dataSourceModel.getUrl());
                 druidDataSource.setUsername(dataSourceModel.getUsername());
                 druidDataSource.setPassword(dataSourceModel.getPassword());
-
-                try {
-                    druidDataSource.setFilters("stat");
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-
-
                 druidDataSource.init();
 
                 logger.info("【" + dataSourceName + "】 数据源正在初始化中");
