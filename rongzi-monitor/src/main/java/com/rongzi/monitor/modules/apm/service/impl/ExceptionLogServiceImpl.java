67d0846cb54c35a88bcab52bcf0745b74d85a881
xulei <abc@123.com>
2018-09-20 11:39:28 +0800

    增加调试语句，显示时间

--- rongzi-monitor/src/main/java/com/rongzi/monitor/modules/apm/service/impl/ExceptionLogServiceImpl.java
+++ rongzi-monitor/src/main/java/com/rongzi/monitor/modules/apm/service/impl/ExceptionLogServiceImpl.java
@@ -8,6 +8,8 @@ import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import com.rongzi.core.mutidatasource.annotion.DataSource;
 import com.rongzi.monitor.modules.apm.model.ExceptionLog;
 import com.rongzi.monitor.modules.apm.service.ExceptionLogService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,8 @@ import java.util.Map;
 
 @Service
 public class ExceptionLogServiceImpl extends ServiceImpl<ExceptionLogMapper, ExceptionLog> implements ExceptionLogService {
+
+    private Logger logger= LoggerFactory.getLogger(ExceptionLogServiceImpl.class);
 
     @Autowired
     private ExceptionLogMapper exceptionLogMapper;
@@ -46,11 +50,19 @@ public class ExceptionLogServiceImpl extends ServiceImpl<ExceptionLogMapper, Exc
     @Override
     public List<Map<String, Object>> getExceptionLogsAllByDatabase(String beginTime, String endTime, Integer systemCode, Integer isReadonly, Page<ExceptionLog> page, String Owner, Integer Status, String orderByField, boolean asc, String flag) {
 
+        Long time=System.currentTimeMillis();
+
         DataSourceContextHolder.setDataSourceType(flag);
 
+        Long time1=System.currentTimeMillis();
         List<Map<String, Object>> data=exceptionLogMapper.findExceptionLogsAll(beginTime, endTime, systemCode, isReadonly, page, Owner, Status,orderByField,asc);
+        Long time2=System.currentTimeMillis();
 
         DataSourceContextHolder.clearDataSourceType();
+        Long time3=System.currentTimeMillis();
+
+        logger.info("包含数据源切换在内的总毫秒数目为："+(time3-time));
+        logger.info("单纯SQL执行消耗时间为："+(time2-time1));
 
         return data;
     }
